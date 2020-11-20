0587a54441c687515b44d70d4430ce87a43e02c0
lqs5858@163.com <365858>
2020-11-20 13:43:31 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'electron-vue'
+}
+</script>
+
+<style lang="less">
+/* CSS */
+#app {
+  background: #0a122d;
+  height: 100%;
+}
+</style>
---
+++ src/renderer/pages/home/index.vue
@@ -0,0 +1,531 @@
+<template>
+  <section class="wrap">
+    <div class="header-wrap flex">
+      <ul>
+        <img id="logo"
+             src="~@/assets/images/leftlogo.png"
+             alt="electron-vue">
+      </ul>
+      <ul class="flex nav-middle">
+        <li>首页</li>
+        <li>行情</li>
+        <li>币币交易</li>
+        <li>法币交易</li>
+        <li>成长课堂</li>
+      </ul>
+      <ul class="flex nav-right">
+        <li>资产</li>
+        <li>订单</li>
+        <li>
+          <img src="~@/assets/images/login-user.png"
+               class="login-user"
+               alt="">
+        </li>
+        <li>APP下载</li>
+        <li>中文</li>
+      </ul>
+    </div>
+    <div class="content">
+      <div class="top flex">
+        <div class="asid">
+          <ul class="flex nav">
+            <li>关注</li>
+            <li class="is-active">USDT</li>
+            <li>免费交易区</li>
+          </ul>
+          <div>
+            <p class="el-input"></p>
+          </div>
+          <div class="table">
+            <ul class="flex header">
+              <li>币对</li>
+              <li>最新价</li>
+              <li>涨跌幅</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+            <ul class="flex row">
+              <li class="symbol">BTC</li>
+              <li class="price">394844</li>
+              <li class="rose">0.53%</li>
+            </ul>
+          </div>
+        </div>
+        <div class="middle flex-column">
+          <div class="kline"></div>
+          <div class="order-input">
+
+          </div>
+        </div>
+        <div class="asid flex-column">
+          <ul class="flex right-header">
+            <li class="handicap-label">盘口</li>
+            <li class="point">4位小数</li>
+          </ul>
+          <div class="handicap-content">
+            <ul class="flex header">
+              <li>价格</li>
+              <li>数量</li>
+              <li>累计</li>
+            </ul>
+            <div class="buy">
+              <ul class="buy-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="buy-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="buy-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="buy-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="buy-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="buy-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="buy-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="buy-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="buy-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="buy-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+            </div>
+            <div class="cur-price flex">
+              <ul class="left flex">
+                <li class="new-price">USDT&nbsp;204.99</li>
+                <li class="rmb">204.00</li>
+              </ul>
+              <p class="more">更多</p>
+            </div>
+            <div class="sell">
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+              <ul class="sell-row flex">
+                <li class="price">74674.98</li>
+                <li>0.738</li>
+                <li>87484</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom-fixed">
+        <ul class="flex row">
+          <li>当前委托</li>
+          <li>历史委托</li>
+          <li>预警记录</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    showHandler () {
+
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.wrap {
+  #logo {
+    width: 194px;
+    height: 31px;
+  }
+  li {
+    color: #fff;
+  }
+  .content {
+    height: calc(100vh - 50px);
+    .top {
+      height: calc(100% - 65px);
+      .table {
+        .row {
+          justify-content: space-between;
+          box-sizing: border-box;
+          padding: 0 5%;
+          li {
+            font-size: 14px;
+            line-height: 20px;
+            font-family: SourceHanSansCN-Normal, SourceHanSansCN;
+            font-weight: 400;
+          }
+          .price {
+            color: #ff004f;
+          }
+          .rose {
+            color: #00c48c;
+          }
+          .symbol {
+            color: #32325d;
+          }
+        }
+        .header {
+          justify-content: space-between;
+          height: 35px;
+          box-sizing: border-box;
+          padding: 0 5%;
+          li {
+            font-size: 14px;
+            font-family: PingFangSC-Regular, PingFang SC;
+            font-weight: 400;
+            color: #959ca3;
+            line-height: 20px;
+            text-shadow: 10px 4px 15px rgba(0, 0, 0, 0.1);
+          }
+        }
+      }
+      .asid {
+        width: 294px;
+        height: 100%;
+        box-sizing: border-box;
+        border: 1px solid #1a2445;
+        .handicap-content {
+          width: 100%;
+          .cur-price {
+            height: 30px;
+            border-top: 1px solid #1a2445;
+            border-bottom: 1px solid #1a2445;
+            box-sizing: border-box;
+            justify-content: space-between;
+            padding: 0 20px;
+            .more {
+              font-size: 14px;
+              font-family: PingFangSC-Regular, PingFang SC;
+              font-weight: 400;
+              color: #acb1c0;
+            }
+            .new-price {
+              font-size: 14px;
+              font-family: PingFangSC-Regular, PingFang SC;
+              font-weight: 400;
+              color: #3fe77b;
+              line-height: 30px;
+            }
+            .rmb {
+              font-size: 12px;
+              font-family: PingFangSC-Regular, PingFang SC;
+              font-weight: 400;
+              color: #acb1c0;
+              line-height: 22px;
+            }
+          }
+
+          .buy,
+          .sell {
+            box-sizing: border-box;
+            padding: 0 20px;
+            flex: 1;
+            li {
+              font-size: 12px;
+              font-family: SFUIDisplay-Medium, SFUIDisplay;
+              font-weight: 500;
+              height: 20px;
+              line-height: 20px;
+            }
+            .sell-row {
+              justify-content: space-between;
+              .price {
+                color: #00c48c;
+              }
+            }
+            .buy-row {
+              justify-content: space-between;
+              .price {
+                color: #ff004f;
+              }
+            }
+          }
+          .header {
+            justify-content: space-between;
+            padding: 0 20px;
+            box-sizing: border-box;
+            height: 30px;
+            border-bottom: 1px solid #1a2445;
+            li {
+              font-size: 14px;
+              font-family: PingFang-SC-Bold, PingFang-SC;
+              font-weight: bold;
+              color: #979ea5;
+              line-height: 20px;
+            }
+          }
+        }
+        .right-header {
+          justify-content: space-between;
+          box-sizing: border-box;
+          padding: 0 20px;
+          width: 100%;
+          height: 30px;
+          border-bottom: 1px solid #1a2445;
+          .point {
+            font-size: 10px;
+            font-family: SFUIDisplay-Medium, SFUIDisplay;
+            font-weight: 500;
+            color: #ffffff;
+            line-height: 12px;
+          }
+          .handicap-label {
+            font-size: 16px;
+            font-family: PingFang-SC-Medium, PingFang-SC;
+            font-weight: 500;
+            color: #979ea5;
+            line-height: 22px;
+          }
+        }
+        .el-input {
+          height: 34px;
+          background: #0d183b;
+          box-shadow: 10px 4px 15px 0px rgba(0, 0, 0, 0.1);
+          border-radius: 22px;
+          margin: 15px 5px 0;
+        }
+        .nav {
+          font-size: 14px;
+          justify-content: center;
+          font-family: PingFangSC-Regular, PingFang SC;
+          font-weight: 400;
+          color: #959ca3;
+          line-height: 46px;
+          text-shadow: 10px 4px 15px rgba(0, 0, 0, 0.1);
+          height: 46px;
+          li {
+            padding: 0 1vw;
+            height: 100%;
+          }
+          .is-active {
+            // padding-bottom: 10px;
+            border-bottom: 1px solid #459eff;
+          }
+        }
+      }
+      .middle {
+        flex: 1;
+        height: 100%;
+        box-sizing: border-box;
+        border-bottom: 1px solid #1a2445;
+        .kline {
+          flex: 1;
+          width: 100%;
+        }
+        .order-input {
+          height: 300px;
+          width: 100%;
+          border-top: 1px solid #1a2445;
+        }
+      }
+    }
+    .bottom-fixed {
+      height: 65px;
+      line-height: 65px;
+      box-sizing: border-box;
+      font-size: 14px;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+      color: #525f7f;
+      .row {
+        padding-left: 1vw;
+      }
+      li {
+        padding-right: 1vw;
+      }
+    }
+  }
+  .header-wrap {
+    width: 100%;
+    height: 50px;
+    box-sizing: border-box;
+    border: 1px solid #1a2445;
+    .nav-middle {
+      flex: 1;
+      justify-content: center;
+      font-size: 16px;
+      font-family: PingFangSC-Semibold, PingFang SC;
+      // font-weight: 600;
+      color: #ffffff;
+      line-height: 22px;
+      & > li:not(:last-child) {
+        padding-right: 3vw;
+      }
+    }
+    .nav-right {
+      font-size: 13px;
+      font-family: PingFangSC-Semibold, PingFang SC;
+      // font-weight: 600;
+      color: #ffffff;
+      line-height: 18px;
+      .login-user {
+        width: 22px;
+        height: 22px;
+      }
+      & > li {
+        padding-right: 1vw;
+      }
+    }
+  }
+}
+</style>
